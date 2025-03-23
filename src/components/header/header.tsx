94347011cb1b4cf9e3c33bf079415803fc6d984d
Lee Freeman <lee.freeman1@gmail.com>
2025-03-23 13:38:15 +0100

    feat: add header component and add to layout

---
+++ src/components/header/header.tsx
@@ -0,0 +1,75 @@
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
+
+export function Header() {
+  return (
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="lg:hidden">
+            <MenuIcon />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent title="Menu" side="left">
+          <p className="sr-only">Menu</p>
+          <Link href="#" prefetch={false}>
+            <span className="sr-only">Home</span>
+          </Link>
+          <div className="grid gap-2 py-6">
+            <Link
+              href="#"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Home
+            </Link>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <NavigationMenu title="Menu" className="hidden lg:flex">
+        <NavigationMenuList>
+          <NavigationMenuLink asChild>
+            <Link
+              href="#"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Home
+            </Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink asChild></NavigationMenuLink>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="ml-auto flex gap-2"></div>
+    </header>
+  );
+}
+
+function MenuIcon() {
+  return (
+    <svg
+      className="h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
+    </svg>
+  );
+}
