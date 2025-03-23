dcfa7c66cac5a864b8d2a297e5dddfbd136d27ad
Lee Freeman <lee.freeman1@gmail.com>
2025-03-23 16:16:19 +0100

    fix: add fallback forimages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.tvmaze.com",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/show-card/show-card.tsx
+++ src/components/show-card/show-card.tsx
@@ -1,12 +1,23 @@
 import { Show } from "@/schema/show";
+import Image from "next/image";
 import Link from "next/link";
 
 export const ShowCard = ({ show }: { show: Show }) => {
   return (
     <Link href={`/show/${show.id}`}>
-      <div>
-        <h1>{show.name}</h1>
-        <img src={show.image.medium} alt={show.name} />
+      <div className="relative">
+        {show.image ? (
+          <Image
+            alt={show.name}
+            width={200}
+            height={280}
+            src={show.image?.medium}
+          />
+        ) : (
+          <div className="w-[200px] h-[280px] bg-gray-200">
+            <p>No image found for {show.name}</p>
+          </div>
+        )}
       </div>
     </Link>
   );
